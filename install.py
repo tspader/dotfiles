04d54d5c61b1c72b783ad6f2b581bddeaab1a783
Thomas Spader <ts185327@ncr.com>
2025-04-02 13:42:22 -0400

    bash

---
+++ install.py
@@ -0,0 +1,35 @@
+import os
+
+appdata = os.getenv('APPDATA')
+
+symlinks = {
+  'bash': {
+    '.bashrc': os.path.expanduser('~'),
+    '.profile': os.path.expanduser('~')
+  },
+  'vscode': {
+    'keybindings.json': os.path.join(appdata, 'Code', 'User'),
+    'settings.json': os.path.join(appdata, 'Code', 'User')
+  }
+}
+
+dotfiles = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
+vscode = os.path.normpath(os.path.join(dotfiles, 'vscode'))
+
+
+def main():
+  for program, program_map in symlinks.items():
+    for target_file, link_path in program_map.items():
+      target_path = os.path.normpath(os.path.join(dotfiles, program, target_file))
+      link_path = os.path.normpath(os.path.join(link_path, target_file))
+
+      print(f'Linking {target_path} -> {link_path}')
+
+      if os.path.exists(link_path):
+        os.remove(link_path)
+
+      os.symlink(target_path, link_path)
+
+
+if __name__ == '__main__':
+  main()
