ebe7b516a0a1e080f216d54153198d0b9baec591
Thomas Spader <thomas.spader@gmail.com>
2025-08-16 13:41:21 -0400

    arch setup

--- linux/arch.sh
+++ linux/arch.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
 pacman -S --needed \
   base-devel \
+  cifs-utils \
   cmake \
   curl \
   docker \
   docker-compose \
+  extra/bind \
   fzf \
   gdb \
   gnupg \
+  httpie \
+  mullvad-vpn \
+  keyd \
+  lazygit \
   less \
+  linux-headers \
   lsof \
   luajit \
   neovim \
   nodejs \
   openssh \
+  ouch \
   python \
-  uv \
+  reflector \
   ripgrep \
+  rsync \
   stow \
+  tealdeer \
   tmux \
   tree \
-  unzip \
-  xclip
+  usbutils \
+  uv \
+  wget \
+  yazi \
+
+pacman -S --needed \
+  plasma-meta
+  xdg-user-dirs \
+  pipewire \
+  pipewire-alsa \
+  pipewire-pulse \
+  pipewire-jack \
+  wireplumber \
+  alacritty \
+  spectacle \
+  ly \
+  okular \
+  firefox \
+  chromium
+
+xdg-user-dirs-update
 
 # Dotfiles
 if [ ! -d "$HOME/.dotfiles" ]; then
@@ -36,30 +65,37 @@ if [ ! -d "$HOME/.dotfiles" ]; then
   git config --global user.email ""
 fi
 
-# SSH
-[ ! -f ~/.ssh/id_ed25519 ] && ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
-systemctl enable --now sshd
 
-# AUR
 if ! id "spader" &>/dev/null; then
     useradd -m -G wheel -s /bin/bash spader
-    # Enable passwordless sudo for wheel group
-    echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+    echo "%wheel ALL=(ALL:ALL): ALL" >> /etc/sudoers
 fi
 
-if ! command -v yay &>/dev/null; then
-    su - spader -c "
-        if [ ! -d ~/yay ]; then
-            git clone https://aur.archlinux.org/yay.git ~/yay
-            cd ~/yay && makepkg -si --noconfirm
-            yay -S --noconfirm tailscale
-        fi
-    "
-fi
+su spader bash -c "
+  # AUR
+  if ! command -v yay &>/dev/null; then
+     if [ ! -d ~/yay ]; then
+         git clone https://aur.archlinux.org/yay.git ~/yay
+         cd ~/yay && makepkg -si --noconfirm
+     fi
+  fi
+  
+  yay -S --noconfirm \
+    tailscale
+  
+  # SSH
+  [ ! -f ~/.ssh/id_ed25519 ] && ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N \"\"
+"
+# systemd
+systemctl enable --now \
+  bluetooth \
+  sshd \
+  tailscaled \
+  docker
 
-# Tailscale
-systemctl enable --now tailscaled
+systemctl --user enable --now \
+  pipewire \
+  pipewire-pulse \
+  wireplumber
+
 tailscale up
-
-# Docker
-systemctl enable --now docker
