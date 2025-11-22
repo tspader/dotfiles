ec4141872153b2a4e3f0c3c6737bd1e188a8ddfc
Thomas Spader <thomas.spader@gmail.com>
2025-11-22 11:57:04 -0500

    gdb: slop

--- gdb/.config/gdb/sp_ps.py
+++ gdb/.config/gdb/sp_ps.py
@@ -6,11 +6,21 @@ class SpPsConfigPrinter:
         self.val = val
 
     def children(self):
-        # Return all fields except io
         for field in self.val.type.fields():
             if field.name == 'io':
-                continue  # Skip io field as requested
-            yield (field.name, self.val[field.name])
+                # Skip io field - we'll handle it differently
+                continue
+            else:
+                yield (field.name, self.val[field.name])
+        
+        # Add io modes as separate fields to avoid nested object issues
+        try:
+            io_val = self.val['io']
+            yield ('io_in_mode', io_val['in']['mode'])
+            yield ('io_out_mode', io_val['out']['mode'])
+            yield ('io_err_mode', io_val['err']['mode'])
+        except:
+            pass
 
     def display_hint(self):
         return 'sp_ps_config_t'
@@ -20,19 +30,41 @@ class SpPsPrinter:
         self.val = val
 
     def children(self):
-        # Return all fields except io
         for field in self.val.type.fields():
             if field.name == 'io':
-                continue  # Skip io field as requested
-            yield (field.name, self.val[field.name])
+                # Skip io field - we'll handle it differently
+                continue
+            else:
+                yield (field.name, self.val[field.name])
+        
+        # Add io modes as separate fields to avoid nested object issues
+        try:
+            io_val = self.val['io']
+            yield ('io_in_mode', io_val['in']['mode'])
+            yield ('io_out_mode', io_val['out']['mode'])
+            yield ('io_err_mode', io_val['err']['mode'])
+        except:
+            pass
 
     def display_hint(self):
         return 'sp_ps_t'
+
+class SpEnvVarPrinter:
+    def __init__(self, val):
+        self.val = val
+
+    def children(self):
+        yield ('key', self.val['key'])
+        yield ('value', self.val['value'])
+
+    def display_hint(self):
+        return 'sp_env_var_t'
 
 def build_pretty_printer():
     pp = gdb.printing.RegexpCollectionPrettyPrinter("sp_ps")
     pp.add_printer('sp_ps_config_t', '^sp_ps_config_t$', SpPsConfigPrinter)
     pp.add_printer('sp_ps_t', '^sp_ps_t$', SpPsPrinter)
+    pp.add_printer('sp_env_var_t', '^sp_env_var_t$', SpEnvVarPrinter)
     return pp
 
 try:
