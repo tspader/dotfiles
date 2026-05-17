05065ed78cc76d64eb50ab84d1b80caf7a9c71e2
Thomas Spader <thomas.spader@gmail.com>
2026-05-17 01:14:47 -0400

    aconfmgr: add

---
+++ aconfmgr/config/30-network.sh
@@ -0,0 +1,4 @@
+
+CopyFile /etc/NetworkManager/conf.d/20-connectivity.conf
+CopyFile /etc/NetworkManager/system-connections/br0-port-enp14s0.nmconnection 600
+CopyFile /etc/NetworkManager/system-connections/br0.nmconnection 600
---
+++ aconfmgr/config/99-unsorted.sh
@@ -0,0 +1,282 @@
+AddPackage act # Run your GitHub Actions locally
+AddPackage age # A simple, modern and secure file encryption tool
+AddPackage alacritty # A cross-platform, GPU-accelerated terminal emulator
+AddPackage alsa-utils # Advanced Linux Sound Architecture - Utilities
+AddPackage amd-ucode # Microcode update image for AMD CPUs
+AddPackage arch-wiki-lite # Arch Wiki without HTML. 1/9 as big, easily searched & viewable on console
+AddPackage atac # A simple API client (postman like) in your terminal
+AddPackage base # Minimal package set to define a basic Arch Linux installation
+AddPackage base-devel # Basic tools to build Arch Linux packages
+AddPackage bat # Cat clone with syntax highlighting and git integration
+AddPackage bc # An arbitrary precision calculator language
+AddPackage bear # A tool to generate compilation database for clang tooling
+AddPackage bind # A complete, highly portable implementation of the DNS protocol
+AddPackage binsider # A TUI for analyzing binaries like a boss
+AddPackage bluedevil # Integrate the Bluetooth technology within KDE workspace and applications
+AddPackage bluez # Daemons for the bluetooth protocol stack
+AddPackage bluez-utils # Development and debugging utilities for the bluetooth protocol stack
+AddPackage bpftrace # High-level tracing language for Linux eBPF
+AddPackage breeze-gtk # Breeze widget theme for GTK 2 and 3
+AddPackage brightnessctl # Lightweight brightness control tool
+AddPackage btop # A monitor of system resources, bpytop ported to C++
+AddPackage ca-certificates # Common CA certificates - default providers
+AddPackage ca-certificates-mozilla # Mozilla's set of trusted CA certificates
+AddPackage ca-certificates-utils # Common CA certificates (utilities)
+AddPackage caddy # Fast web server with automatic HTTPS
+AddPackage cargo-dist # Shippable application packaging for Rust
+AddPackage ccache # Compiler cache that speeds up recompilation by caching previous compilations
+AddPackage chromium # A web browser built for speed, simplicity, and security
+AddPackage cloc # Count lines of code
+AddPackage cmake # A cross-platform open-source make system
+AddPackage cowsay # Configurable talking cow (and a bit more)
+AddPackage cpio # A tool to copy files into or out of a cpio or tar archive
+AddPackage cuda # NVIDIA's GPU programming toolkit
+AddPackage curl # command line tool and library for transferring data with URLs
+AddPackage direnv # A shell extension that manages your environment
+AddPackage dnsmasq # Lightweight, easy to configure DNS forwarder and DHCP server
+AddPackage docker # Pack, ship and run any application as a lightweight container
+AddPackage docker-buildx # Docker CLI plugin for extended build capabilities with BuildKit
+AddPackage docker-compose # Fast, isolated development environments using Docker
+AddPackage dolphin # KDE File Manager
+AddPackage dos2unix # Text file format converter
+AddPackage dust # A more intuitive version of du in rust
+AddPackage expac # alpm data (pacman database) extraction utility
+AddPackage firefox # Fast, Private & Safe Web Browser
+AddPackage fzf # Command-line fuzzy finder
+AddPackage gdb # The GNU Debugger
+AddPackage gdb-dashboard # Modular visual interface for GDB in Python
+AddPackage ghostty # Fast, native, feature-rich terminal emulator pushing modern features
+AddPackage git # the fast distributed version control system
+AddPackage git-delta # Syntax-highlighting pager for git and diff output
+AddPackage git-filter-repo # Quickly rewrite git repository history (filter-branch replacement)
+AddPackage git-lfs # Git extension for versioning large files
+AddPackage github-cli # The GitHub CLI
+AddPackage glib2-devel # Low level core library - development tools
+AddPackage glow # Command-line markdown renderer
+AddPackage gopls # Language server for Go programming language
+AddPackage guvcview # Simple GTK+ interface for capturing and viewing video from v4l2 devices
+AddPackage htop # Interactive process viewer
+AddPackage httpie # human-friendly CLI HTTP client for the API era
+AddPackage hugo # Fast and Flexible Static Site Generator in Go
+AddPackage hypnotix # IPTV streaming application with support for live TV, movies and series
+AddPackage hyprland # a highly customizable dynamic tiling Wayland compositor
+AddPackage hyprlauncher # A multipurpose and versatile launcher / picker for Hyprland
+AddPackage hyprpicker # A wlroots-compatible Wayland color picker that does not suck
+AddPackage hyprpolkitagent # Simple polkit authentication agent for Hyprland, written in QT/QML
+AddPackage ibus # Intelligent input bus for Linux/Unix
+AddPackage inetutils # A collection of common network programs
+AddPackage irssi # Modular text mode IRC client with Perl scripting
+AddPackage jq # Command-line JSON processor
+AddPackage kde-gtk-config # Syncs KDE settings to GTK applications
+AddPackage kdeplasma-addons # All kind of addons to improve your Plasma experience
+AddPackage keyd # A key remapping daemon for linux
+AddPackage kinfocenter # A utility that provides information about a computer system
+AddPackage kmon # Linux kernel manager and activity monitor
+AddPackage kscreen # KDE screen management software
+AddPackage kwalletmanager # Wallet management tool
+AddPackage lazygit # Simple terminal UI for git commands
+AddPackage less # A terminal based program for viewing text files
+AddPackage libheif # An HEIF and AVIF file format decoder and encoder
+AddPackage libwebsockets # C library for websocket clients and servers
+AddPackage linux # The Linux kernel and modules
+AddPackage linux-firmware # Firmware files for Linux - Default set
+AddPackage linux-headers # Headers and scripts for building modules for the Linux kernel
+AddPackage lld # Linker from the LLVM project
+AddPackage lsd # Modern ls with a lot of pretty colors and awesome icons
+AddPackage lsof # Lists open files for running Unix processes
+AddPackage lua-language-server # Lua Language Server coded by Lua
+AddPackage man-db # A utility for reading man pages
+AddPackage man-pages # Linux man pages
+AddPackage mingw-w64-binutils # Cross binutils for the MinGW-w64 cross-compiler
+AddPackage mingw-w64-crt # MinGW-w64 CRT for Windows
+AddPackage mingw-w64-gcc # Cross GCC for the MinGW-w64 cross-compiler
+AddPackage mingw-w64-headers # MinGW-w64 headers for Windows
+AddPackage mingw-w64-winpthreads # MinGW-w64 winpthreads library
+AddPackage mold # A Modern Linker
+AddPackage mtools # A collection of utilities to access MS-DOS disks
+AddPackage musl # Lightweight implementation of C standard library
+AddPackage nano # Pico editor clone with enhancements
+AddPackage nasm # 80x86 assembler designed for portability and modularity
+AddPackage ncdu # Disk usage analyzer with an ncurses interface
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage networkmanager # Network connection manager and user applications
+AddPackage nginx # Lightweight HTTP server and IMAP/POP3 proxy server
+AddPackage ninja # Small build system with a focus on speed
+AddPackage nmap # Utility for network discovery and security auditing
+AddPackage noto-fonts-cjk # Google Noto CJK fonts
+AddPackage nss-mdns # glibc plugin providing host name resolution via mDNS
+AddPackage ntfs-3g # NTFS filesystem driver and utilities
+AddPackage nvidia-container-toolkit # NVIDIA container toolkit
+AddPackage nvidia-open # NVIDIA open kernel modules
+AddPackage nvidia-settings # Tool for configuring the NVIDIA graphics driver
+AddPackage nvidia-utils # NVIDIA drivers utilities
+AddPackage nvme-cli # NVM-Express user space tooling for Linux
+AddPackage obs-studio # Free, open source software for live streaming and recording
+AddPackage obsidian # A powerful knowledge base that works on top of a local folder of plain text Markdown files
+AddPackage openssh # SSH protocol implementation for remote login, command execution and file transfer
+AddPackage otf-commit-mono-nerd # Patched font Commit Mono from nerd fonts library
+AddPackage ouch # A command line utility for easily compressing and decompressing files and directories
+AddPackage pacman-contrib # Contributed scripts and tools for pacman systems
+AddPackage pahole # Type info tools
+AddPackage pass # Stores, retrieves, generates, and synchronizes passwords securely
+AddPackage pipewire-alsa # Low-latency audio/video router and processor - ALSA configuration
+AddPackage pipewire-jack # Low-latency audio/video router and processor - JACK replacement
+AddPackage plasma-desktop # KDE Plasma Desktop
+AddPackage plasma-meta # Meta package to install KDE Plasma
+AddPackage plasma-nm # Plasma applet written in QML for managing network connections
+AddPackage plasma-pa # Plasma applet for audio volume management using PulseAudio
+AddPackage plasma-systemmonitor # An interface for monitoring system sensors, process information and other system resources
+AddPackage plasma-workspace # KDE Plasma Workspace
+AddPackage powerdevil # Manages the power consumption settings of a Plasma Shell
+AddPackage prometheus-node-exporter # Exporter for machine metrics
+AddPackage python # The Python programming language
+AddPackage qemu-full # A full QEMU setup
+AddPackage qt6-connectivity # Provides access to Bluetooth hardware
+AddPackage qt6-wayland # Provides APIs for Wayland
+AddPackage reflector # A Python 3 module and script to retrieve and filter the latest Pacman mirror list.
+AddPackage renderdoc # OpenGL and Vulkan debugging tool
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+AddPackage rpmextract # Script to convert or extract RPM archives (contains rpm2cpio)
+AddPackage rsync # A fast and versatile file copying tool for remote and local files
+AddPackage ruby # An object-oriented language for quick and easy programming
+AddPackage ruff # An extremely fast Python linter, written in Rust
+AddPackage rustup # The Rust toolchain installer
+AddPackage simh # simulator for assorted historical computers, from Altair to VAX
+AddPackage sl # Steam Locomotive runs across your terminal when you type "sl" as you meant to type "ls"
+AddPackage sndio # A small audio and MIDI framework
+AddPackage solaar # Linux device manager for a wide range of Logitech devices
+AddPackage spectacle # KDE screenshot capture utility
+AddPackage spirv-headers # SPIR-V header files and registry
+AddPackage spotify-player # A command driven spotify player
+AddPackage sshfs # FUSE client based on the SSH File Transfer Protocol
+AddPackage steam # Valve's digital software delivery system
+AddPackage stow # Manage installation of multiple softwares in the same directory tree
+AddPackage strace # A diagnostic, debugging and instructional userspace tracer
+AddPackage sudo # Give certain users the ability to run some commands as root
+AddPackage sysstat # a collection of performance monitoring tools (iostat,isag,mpstat,pidstat,sadf,sar)
+AddPackage systemsettings # KDE system manager for hardware, software, and workspaces
+AddPackage tailscale # A mesh VPN that makes it easy to connect your devices, wherever they are.
+AddPackage tcc # Tiny C Compiler (tinycc)
+AddPackage terraform # HashiCorp tool for building and updating infrastructure as code idempotently
+AddPackage thefuck # Magnificent app which corrects your previous console command
+AddPackage tigervnc # Suite of VNC servers and clients based on the VNC 4 branch of TightVNC
+AddPackage tiny # A terminal IRC client written in Rust
+AddPackage tmux # Terminal multiplexer
+AddPackage trash-cli # Command line trashcan (recycle bin) interface
+AddPackage tree # A directory listing program displaying a depth indented list of files
+AddPackage tree-sitter-cli # CLI tool for developing, testing, and using Tree-sitter parsers
+AddPackage ttf-iosevka-nerd # Patched font Iosevka from nerd fonts library
+AddPackage ttf-jetbrains-mono # Typeface for developers, by JetBrains
+AddPackage ttf-jetbrains-mono-nerd # Patched font JetBrains Mono from nerd fonts library
+AddPackage unzip # For extracting and viewing files in .zip archives
+AddPackage upx # Extendable, high-performance executable packer for several executable formats
+AddPackage usbutils # A collection of USB tools to query connected USB devices
+AddPackage uv # An extremely fast Python package installer and resolver written in Rust
+AddPackage valkey # An in-memory database that persists on disk
+AddPackage vim # Vi Improved, a highly configurable, improved version of the vi text editor
+AddPackage virt-manager # Desktop user interface for managing virtual machines
+AddPackage volk # Meta loader for Vulkan API
+AddPackage vulkan-extra-layers # Extra layers for Vulkan development
+AddPackage vulkan-extra-tools # Vulkan Configurator and Installation Analyzer
+AddPackage vulkan-headers # Vulkan header files and API registry
+AddPackage vulkan-utility-libraries # Vulkan Utility Libraries
+AddPackage vulkan-validation-layers # Vulkan Validation Layers
+AddPackage wabt # The WebAssembly Binary Toolkit: a suite of tools for WebAssembly
+AddPackage wasmer # Universal Binaries Powered by WebAssembly
+AddPackage wasmtime # Standalone JIT-style runtime for WebAssembly, using Cranelift
+AddPackage waybar # Highly customizable Wayland bar for Sway and Wlroots based compositors
+AddPackage wayland-protocols # Specifications of extended Wayland protocols
+AddPackage wezterm # A GPU-accelerated cross-platform terminal emulator and multiplexer
+AddPackage wget # Network utility to retrieve files from the web
+AddPackage whois # Intelligent WHOIS client
+AddPackage wl-clipboard # Command-line copy/paste utilities for Wayland
+AddPackage wpa_supplicant # A utility providing key negotiation for WPA wireless networks
+AddPackage xclip # Command line interface to the X11 clipboard
+AddPackage xdg-desktop-portal-hyprland # xdg-desktop-portal backend for hyprland
+AddPackage xmlto # Convert xml to many other formats
+AddPackage yazi # Blazing fast terminal file manager written in Rust, based on async I/O
+AddPackage ydotool # Generic command-line automation tool (no X!)
+AddPackage zellij # A terminal multiplexer
+AddPackage zsh # A very advanced and programmable command interpreter (shell) for UNIX
+AddPackage fastfetch # A feature-rich and performance oriented neofetch like system information tool
+
+AddPackage --foreign browsh-bin # A fully-modern text-based browser, rendering to TTY and browsers
+AddPackage --foreign codex-desktop-bin # OpenAI Codex Desktop (prepatched Linux payload)
+AddPackage --foreign dotool # Command to simulate input anywhere
+AddPackage --foreign elephant-all # elephant + all official elephant providers
+AddPackage --foreign google-chrome # The popular web browser by Google (Stable Channel)
+AddPackage --foreign kdotool # A xdotool clone for KDE Wayland
+AddPackage --foreign lazysql-bin # A cross-platform TUI database management tool written in Go
+AddPackage --foreign lmstudio # Discover, download, and run local LLMs
+AddPackage --foreign localsend-bin # An open source cross-platform alternative to AirDrop
+AddPackage --foreign mullvad-vpn-bin # The Mullvad VPN client app for desktop (desktop application)
+AddPackage --foreign nvimpager # Use nvim as a pager to view manpages, diffs, etc with nvim's syntax highlighting
+AddPackage --foreign opencode-bin # The AI coding agent built for the terminal.
+AddPackage --foreign paru-git # Feature packed AUR helper
+AddPackage --foreign prometheus-nvidia-gpu-exporter-git # Prometheus exporter for NVIDIA GPU metrics
+AddPackage --foreign quickemu-git # Quickly create and run optimised Windows, macOS and Linux desktop virtual machines
+AddPackage --foreign slack-desktop # Slack Desktop (Beta) for Linux
+AddPackage --foreign slippi-launcher # The way to play Slippi Online and watch replays.
+AddPackage --foreign spotify # A proprietary music streaming service
+AddPackage --foreign walker # wayland application runner
+AddPackage --foreign whatsapp-linux-desktop # An unofficial WhatsApp Linux Desktop application
+AddPackage --foreign zulip-desktop-bin # Real-time team chat based on the email threading model (bin)
+
+
+CopyFile /etc/environment
+CopyFile /etc/nsswitch.conf
+CopyFile /etc/shells
+CopyFile /etc/sudoers
+CopyFile /etc/conf.d/prometheus-node-exporter
+CopyFile /etc/cdi/nvidia.yaml
+CopyFile /etc/dconf/db/ibus
+CopyFile /etc/makepkg.conf.d/fortran.conf
+CopyFile /etc/mkinitcpio.d/linux.preset
+CopyFile /etc/qemu/bridge.conf
+CopyFile /etc/sddm.conf.d/kde_settings.conf
+CopyFile /etc/sysctl.d/10-disable-ipv6.conf
+CopyFile /etc/sysctl.d/99-swappiness.conf
+CopyFile /etc/systemd/system.conf
+CopyFile /etc/systemd/system/-.slice.d/50-oomd.conf
+CopyFile /etc/systemd/system/user@.service.d/50-oomd.conf
+CreateLink /etc/keyd/default.conf ../../home/spader/.dotfiles/keyd/etc/keyd/default.conf
+CopyFile /etc/libvirt/nwfilter/allow-arp.xml
+CopyFile /etc/libvirt/nwfilter/allow-dhcp-server.xml
+CopyFile /etc/libvirt/nwfilter/allow-dhcp.xml
+CopyFile /etc/libvirt/nwfilter/allow-dhcpv6-server.xml
+CopyFile /etc/libvirt/nwfilter/allow-dhcpv6.xml
+CopyFile /etc/libvirt/nwfilter/allow-incoming-ipv4.xml
+CopyFile /etc/libvirt/nwfilter/allow-incoming-ipv6.xml
+CopyFile /etc/libvirt/nwfilter/allow-ipv4.xml
+CopyFile /etc/libvirt/nwfilter/allow-ipv6.xml
+CopyFile /etc/libvirt/nwfilter/clean-traffic-gateway.xml
+CopyFile /etc/libvirt/nwfilter/clean-traffic.xml
+CopyFile /etc/libvirt/nwfilter/no-arp-ip-spoofing.xml
+CopyFile /etc/libvirt/nwfilter/no-arp-mac-spoofing.xml
+CopyFile /etc/libvirt/nwfilter/no-arp-spoofing.xml
+CopyFile /etc/libvirt/nwfilter/no-ip-multicast.xml
+CopyFile /etc/libvirt/nwfilter/no-ip-spoofing.xml
+CopyFile /etc/libvirt/nwfilter/no-ipv6-multicast.xml
+CopyFile /etc/libvirt/nwfilter/no-ipv6-spoofing.xml
+CopyFile /etc/libvirt/nwfilter/no-mac-broadcast.xml
+CopyFile /etc/libvirt/nwfilter/no-mac-spoofing.xml
+CopyFile /etc/libvirt/nwfilter/no-other-l2-traffic.xml
+CopyFile /etc/libvirt/nwfilter/no-other-rarp-traffic.xml
+CopyFile /etc/libvirt/nwfilter/qemu-announce-self-rarp.xml
+CopyFile /etc/libvirt/nwfilter/qemu-announce-self.xml
+CopyFile /etc/libvirt/qemu/networks/default.xml
+CopyFile /etc/libvirt/qemu/archlinux.xml 600
+CopyFile /etc/libvirt/qemu/microos-lab.xml 600
+CopyFile /etc/libvirt/storage/default.xml 600
+CopyFile /etc/libvirt/storage/dl.xml 600
+CopyFile /etc/libvirt/storage/iso.xml 600
+CopyFile /etc/libvirt/storage/storage-1.xml 600
+CopyFile /etc/libvirt/storage/storage.xml 600
+CopyFile /etc/libvirt/storage/windows-11-ltsc.xml 600
+CreateLink /etc/libvirt/qemu/networks/autostart/default.xml /etc/libvirt/qemu/networks/default.xml
+CreateLink /etc/libvirt/storage/autostart/default.xml /etc/libvirt/storage/default.xml
+CreateLink /etc/libvirt/storage/autostart/dl.xml /etc/libvirt/storage/dl.xml
+CreateLink /etc/libvirt/storage/autostart/iso.xml /etc/libvirt/storage/iso.xml
+CreateLink /etc/libvirt/storage/autostart/storage-1.xml /etc/libvirt/storage/storage-1.xml
+CreateLink /etc/libvirt/storage/autostart/storage.xml /etc/libvirt/storage/storage.xml
+CreateLink /etc/libvirt/storage/autostart/windows-11-ltsc.xml /etc/libvirt/storage/windows-11-ltsc.xml
